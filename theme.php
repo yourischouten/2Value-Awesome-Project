753e36460c71dd0a7e168c02fc106b682ea5dca7
Youri Schouten <yschouten@beurtvaartadres.nl>
2011-12-21 16:13:53 +0100

    nieuw thema

---
+++ theme.php
@@ -0,0 +1,14 @@
+<?php
+<html>
+	<head>
+		<link rel="styleSheet" href="style.css" type="text/css">
+	</head>
+	<body>
+		<div id="wrp">
+			<div id="wrpT">
+				<?php echo "Een nieuwe thema "; ?>
+			</div>
+		</div>
+	</body>
+
+</html>
